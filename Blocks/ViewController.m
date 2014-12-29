0f8a1a47a41b9f89d0fd5b4459f73d33556907ac
jainsakshi22 <jainsakshi22@gmail.com>
2014-12-29 21:09:55 +0530

    Basic practice on creation of block

--- Blocks/ViewController.m
+++ Blocks/ViewController.m
@@ -17,6 +17,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+   __block int number;
+    
+    float (^blockAdd)(int a, float b) = ^(int a,float b)
+    {
+        //int number;
+        number = a;
+         NSLog(@"number inside block = %i",number);
+        NSLog(@"Values are %i and %f",a,b);
+        return (a+b);
+    };
+    
+    float result = blockAdd (10,20.5);
+    NSLog(@"result %f",result);
+     NSLog(@"number outside block= %i",number); //giving random output if same variable_name is also declared in block.
 }
 
 - (void)didReceiveMemoryWarning {
